63749787a2cdeeeef75b25023da71d82bbcbbc57
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2025-11-19 12:16:19 +0100

    activated buttons between main and stats activitys

--- app/src/main/java/com/example/blackjack/MainActivity.kt
+++ app/src/main/java/com/example/blackjack/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.blackjack
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+        val statsButton = findViewById<Button>(R.id.btn_stat)
+        statsButton.setOnClickListener {
+            val intent = Intent(this, StatsActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/blackjack/StatsActivity.kt
+++ app/src/main/java/com/example/blackjack/StatsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.blackjack
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,10 @@ class StatsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val backButton = findViewById<Button>(R.id.btn_stat_back)
+        backButton.setOnClickListener {
+            finish()
         }
     }
 }
