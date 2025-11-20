67d301217ffcbe4f97f7071017faad123623b537
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2025-11-20 11:23:31 +0100

    added basic functions

--- app/src/main/java/com/example/blackjack/MainActivity.kt
+++ app/src/main/java/com/example/blackjack/MainActivity.kt
@@ -2,12 +2,8 @@ package com.example.blackjack
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.blackjack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +14,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var dealerSum: TextView
     lateinit var playerSum: TextView
 
-    var playerCards = mutableListOf<Int>()
-    var dealerCards = mutableListOf<Int>()
+    var playerCardsList = mutableListOf<Int>()
+    var dealerCardsList = mutableListOf<Int>()
     var gameOver = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +35,7 @@ class MainActivity : AppCompatActivity() {
             newGame()
         }
         binding.btnDraw.setOnClickListener {
-            playerCards.add(drawCard())
+            playerCardsList.add(drawCard())
 
             showCards()
         }
@@ -52,20 +48,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun newGame(){
         gameOver = false
-        playerCards.add(drawCard())
-        playerCards.add(drawCard())
 
-        dealerCards.add(drawCard())
+        playerCardsList.clear()
+        dealerCardsList.clear()
+
+        playerCardsList.add(drawCard())
+        playerCardsList.add(drawCard())
+
+        dealerCardsList.add(drawCard())
 
         showCards()
     }
 
     private fun showCards() {
-        binding.tvPlayerCard1.text = playerCards.toString()
-        binding.tvPlayerSumNum.text = playerCards.sum().toString()
+        binding.tvPlayerCard1.text = playerCardsList.toString()
+        binding.tvPlayerSumNum.text = playerCardsList.sum().toString()
 
-        binding.tvDealerCard1.text = dealerCards.toString()
-        binding.tvDealerSumNum.text = dealerCards.sum().toString()
+        binding.tvDealerCard1.text = dealerCardsList.toString()
+        binding.tvDealerSumNum.text = dealerCardsList.sum().toString()
     }
 
 
