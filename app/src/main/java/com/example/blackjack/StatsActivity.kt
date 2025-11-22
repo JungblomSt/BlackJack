8aa43198b88e6df3758391ae177fc8aa069756ec
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2025-11-22 16:06:21 +0100

    added basic functions, binding

--- app/src/main/java/com/example/blackjack/StatsActivity.kt
+++ app/src/main/java/com/example/blackjack/StatsActivity.kt
@@ -6,17 +6,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.blackjack.databinding.ActivityMainBinding
+import com.example.blackjack.databinding.ActivityStatsBinding
 
 class StatsActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityStatsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_stats)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityStatsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val backButton = findViewById<Button>(R.id.btn_stat_back)
         backButton.setOnClickListener {
             finish()
