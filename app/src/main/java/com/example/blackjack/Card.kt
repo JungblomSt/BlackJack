ae8abe7836cf52a4fc7643b9b2d0bc1fa94c44bd
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2025-11-25 12:33:06 +0100

    added card logic

---
+++ app/src/main/java/com/example/blackjack/Card.kt
@@ -0,0 +1,7 @@
+package com.example.blackjack
+
+data class Card(
+    val number: String,
+    val suit: String,
+    val value: Int
+)
---
+++ app/src/main/java/com/example/blackjack/Deck.kt
@@ -0,0 +1,7 @@
+package com.example.blackjack
+
+class Deck {
+
+    val numbers = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+    val suits = listOf("♥","♦","♣","♠",)
+}
