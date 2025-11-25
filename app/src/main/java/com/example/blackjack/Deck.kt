6016a179325f7ef932ffcee64a14ba0b9a21634d
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2025-11-25 13:01:21 +0100

    added card logic

--- app/src/main/java/com/example/blackjack/Deck.kt
+++ app/src/main/java/com/example/blackjack/Deck.kt
@@ -4,4 +4,21 @@ class Deck {
 
     val numbers = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
     val suits = listOf("♥","♦","♣","♠",)
+
+    val cards = mutableListOf<Card>()
+
+    init {
+        for (suit in suits) {
+            for (number in numbers){
+                val value = when (number){
+                    "A" -> 11
+                    "J", "Q", "K" -> 10
+                    else -> number.withIndex() + 1
+                }
+                cards.add(Card(number, suit, value as Int))
+            }
+
+        }
+        cards.shuffle()
+    }
 }
